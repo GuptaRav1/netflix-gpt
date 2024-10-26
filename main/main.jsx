0884c805815d3ff103ac040db82b7bd82a71bc0f
Ravi Gupta <ravigupta3282@gmail.com>
2024-10-27 02:27:07 +0530

    added logo and bg image

--- main/main.jsx
+++ main/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Body from '../src/components/Body'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Body />
   </StrictMode>,
 )
---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/74d734ca-0eab-4cd9-871f-bca01823d872/web/IN-en-20241021-TRIFECTA-perspective_2277eb50-9da3-4fdf-adbe-74db0e9ee2cf_large.jpg" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Login
